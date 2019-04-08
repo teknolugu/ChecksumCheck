40659883c366db07bffaf94a9bca5f23f4675801
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-04-08 11:09:35 +0700

    Add an icon

--- ChecksumCheck/My Project/Resources.Designer.vb
+++ ChecksumCheck/My Project/Resources.Designer.vb
@@ -103,6 +103,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property baseline_code_white_18dp() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("baseline_code_white_18dp", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property baseline_folder_open_white_18dp() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("baseline_folder_open_white_18dp", resourceCulture)
